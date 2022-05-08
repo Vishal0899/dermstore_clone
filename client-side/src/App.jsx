5e9d3d210496266e883d37d67b7d2893e27a41a8
iMrihan <mhd.khan.rihan@gmail.com>
2022-05-08 17:22:56 +0530

    Checkout and Product page CSS corrected

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,21 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import { Cart } from "./Components/cartprovider/cart";
 
 import { Middle } from "./Components/Middle";
 import { second_extra } from "./Components/slider";
 import { Login } from "./Components/SignUp&Login/Login";
 import { SignUp } from "./Components/SignUp&Login/SignUp";
+import { ProductPage } from "./Components/productPage/Productpage";
+import { Wishlist } from "./Components/wishlistPage/Wishlist";
+import { CheckoutPage } from "./Components/cartprovider/CheckoutPage";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
 	return (
 		<div>
 			<Routes>
 				<Route path="/" element={<Middle slides={second_extra} />} />
-				<Route path="/cart" element={<Cart />} />
+				<Route
+					path="/checkoutPage"
+					element={
+						<PrivateRoute>
+							<CheckoutPage />
+						</PrivateRoute>
+					}
+				/>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signUp" element={<SignUp />} />
+				<Route path="/productPage" element={<ProductPage />} />
+				<Route
+					path="/wishlist"
+					element={
+						<PrivateRoute>
+							<Wishlist />
+						</PrivateRoute>
+					}
+				/>
 			</Routes>
 		</div>
 	);
